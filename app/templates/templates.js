66d4a6af4df9c94ca562b7b893687514829f8197
marb <marb61a@gmail.com>
2015-07-02 08:10:20 +0000

    added templates folder and files

---
+++ app/templates/templates.js
@@ -0,0 +1,14 @@
+angular.module('templateStore.templates', [ngRoute])
+
+.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+		when('/templates', {
+			templateUrl: 'templates/templates.html',
+			controller: 'TemplatesCtrl'
+		});
+    
+}])
+
+.controller('TemplatesCtrl', ['$scope', function(){
+    console.log($scope);
+}]);
